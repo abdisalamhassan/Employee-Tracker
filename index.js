35c867ce8f5db07accc200cce21c47aeae8c450f
abdisalamhassan <95972020+abdisalamhassan@users.noreply.github.com>
2022-03-20 15:25:14 -0400

    Add Employee

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function start(){
                    "Add Department"
                ]
            }
-      ])..then(function (answer) {
+      ]).then(function (answer) {
         switch (answer.mainMenu) {
           case "View All Employees":
             viewAllEmployees();
@@ -66,48 +66,138 @@ function start(){
       });
 }
 
-function updateEmployee() {
-    connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function(err, res) {
-    
-     if (err) throw err
-     console.log(res)
-    inquirer.prompt([
-          {
-            name: "lastName",
-            type: "rawlist",
-            choices: function() {
-              var lastName = [];
-              for (var i = 0; i < res.length; i++) {
-                lastName.push(res[i].last_name);
-              }
-              return lastName;
+function addDept(){
+    inquirer
+       .prompt([
+           {
+               type: "list",
+               name: "view",
+               message: "select one to view:"
+               choices: ["All Employees", "By department", "By role"]
+           }
+       ]).then(function(answer){
+           connection.query(
+               "INSERT INTO role SET ?",
+            {
+                title: answer.role,
+                salary: answer.salary,
+                department_id: answer.department_id
             },
-            message: "What is the Employee's last name? ",
-          },
-          {
-            name: "role",
-            type: "rawlist",
-            message: "What is the Employees new title? ",
-            choices: selectRole()
-          },
-      ]).then(function(val) {
-        var roleId = selectRole().indexOf(val.role) + 1
-        connection.query("UPDATE employee SET WHERE ?", 
-        {
-          last_name: val.lastName
-           
-        }, 
-        {
-          role_id: roleId
-           
-        }, 
-        function(err){
-            if (err) throw err
-            console.table(val)
-            startPrompt()
-        })
-  
-    });
-  });
+            function(err){
+                if(err) throw err;
+                console.log("_____________________________");
+                console.log("Employee Roles updated with" + answer.role);
+                console.log("______________________________");
+                start();
+            }
+           )
+       })
+}
 
-  }
+function addEmploeeRole(){
+    connection.query("SELECT * FROM role", function(err, results){
+        if(err) throw err;
+        inquirer.prompt([
+            {
+                name: "firstName",
+                type: "input",
+                message: "Please enter employee first name"
+            },
+            {
+                name: "LastName",
+                type: "input",
+                message: "Enter employee's last name",
+            },
+            {
+                name: "role",
+                type: "list",
+                choices: function(){
+                    choices: function(value){
+                        if(isNaN(value)=== false){
+                            return true;
+                        }
+                        return false;
+                    }
+                    return false;
+                },
+                message: "Enter manager ID",
+                default: "1"
+            }
+        ]).then(function(answer){
+            connection.query(
+                "INSERT INTO employee SET ?",
+                {
+                    first_name: answer.firstName,
+                    last_name: answer.lastName,
+                    role_id: answer.role,
+                    manager_id: answer.manager 
+                }
+            )
+            console.log("_________________"),
+            console.log("Employee Added Successfully"),
+            console.log("__________________");
+            start()
+        });
+    });
+}
+
+function updateEmployee(){
+    connection.query("SELECT * FROM employee",
+       function(err, results){
+           if(err) throw err;
+           inquirer
+           .prompt([
+               {
+                   name: "choice",
+                   type: "list",
+                   choices: function(){
+                       let choiceARR = [];
+                       for(i=0; i< results.length; i++)
+                    {
+                        choiceArr.push (results[i].last_name);
+                    }
+                    return choiceArr;
+                   },
+                   message: "Select employee to update"
+               }
+           ])
+        .then(function(answer){
+            const saveName = answer.choice;
+
+            connection.query("SELECT * FROM employee",
+            function(err,results){
+                if(err) throw err;
+            inquirer
+            .prompt([
+                {
+                    name:"role",
+                    type: "list",
+                    choices: function(){
+                        var choiceArr = [];
+                        for(i=0; i< resukts.length; i++){
+                            choiceArr.push(results[i].role_id)
+                        }
+                        return choiceArr;
+                    },
+                    message: "Select title"
+                },
+                {
+                    name: "manager",
+                    type: "number",
+                    validate: function(value){
+                        if(isNan(value)=== false){
+                            return true;
+                        }
+                        return false;
+                    },
+                    message: "Enter new manager ID",
+                    default: "1"
+                }
+            ])
+            })
+        })
+       }
+    )
+}
+
+  
